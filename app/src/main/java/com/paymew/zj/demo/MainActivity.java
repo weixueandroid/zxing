8f585cab5475de1620db4f772402e9aed8cc6c42
zhangjian <1830699177@qq.com>
2018-04-03 16:27:04 +0800

    第一次提交

---
+++ app/src/main/java/com/paymew/zj/demo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.paymew.zj.demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.CaptureActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvResult;
+    private Button btQR;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvResult = findViewById(R.id.tv_result);
+        btQR = findViewById(R.id.bt_qr);
+        btQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MainActivity.this, CaptureActivity.class),100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK){
+            String result = data.getStringExtra("result");
+            Log.e("======",result);
+            tvResult.setText(result);
+        }
+    }
+}
