ccb2ddaa32234d37d10351058c89b70787e52389
zhangjian <1830699177@qq.com>
2020-05-06 18:23:47 +0800

    更新Gradle版本号，更新jar，更新SDK版本

--- app/src/main/java/com/paymew/zj/demo/MainActivity.java
+++ app/src/main/java/com/paymew/zj/demo/MainActivity.java
@@ -1,12 +1,13 @@
 package com.paymew.zj.demo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.CaptureActivity;
 
